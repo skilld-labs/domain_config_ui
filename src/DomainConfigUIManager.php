aeb471e6cd772a8cb6511c7779807a9fc027f4e4
Andy Postnikov <apostnikov@gmail.com>
2018-07-26 20:33:44 +0300

    Minimize useless function calls

--- src/DomainConfigUIManager.php
+++ src/DomainConfigUIManager.php
@@ -2,19 +2,40 @@
 
 namespace Drupal\domain_config_ui;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Domain Config UI manager.
+ *
+ * @todo Find better way to cache domain and language.
  */
 class DomainConfigUIManager {
+  use DependencySerializationTrait {
+    __wakeup as defaultWakeup;
+    __sleep as defaultSleep;
+  }
 
   /**
-   * A RequestStack instance.
+   * The request stack.
    *
-   * @var \Symfony\Component\HttpFoundation\Request
+   * @var \Symfony\Component\HttpFoundation\RequestStack
    */
-  protected $request;
+  protected $requestStack;
+
+  /**
+   * A cached domain.
+   *
+   * @var string
+   */
+  protected $domain;
+
+  /**
+   * A cached language.
+   *
+   * @var string
+   */
+  protected $language;
 
   /**
    * Constructs DomainConfigUIManager object.
@@ -23,7 +44,7 @@ class DomainConfigUIManager {
    *   The request stack.
    */
   public function __construct(RequestStack $request_stack) {
-    $this->request = $request_stack->getCurrentRequest();
+    $this->requestStack = $request_stack;
   }
 
   /**
@@ -31,6 +52,9 @@ class DomainConfigUIManager {
    *
    * @param string $name
    *   The config name.
+   *
+   * @return string
+   *   The prefixed config name.
    */
   public function getSelectedConfigName($name) {
     if ($domain_id = $this->getSelectedDomainId()) {
@@ -45,26 +69,68 @@ class DomainConfigUIManager {
 
   /**
    * Get the selected domain ID.
+   *
+   * @return string
+   *   The selected domain from query string or session.
    */
   public function getSelectedDomainId() {
-    if ($domain = $this->request->get('domain_config_ui_domain')) {
-      return $domain;
+    if (isset($this->domain)) {
+      return $this->domain;
     }
-    if (isset($_SESSION['domain_config_ui_domain'])) {
-      return $_SESSION['domain_config_ui_domain'];
+    $request = $this->requestStack->getCurrentRequest();
+    if ($request->query->has('domain_config_ui_domain')) {
+      $this->domain = $request->query->get('domain_config_ui_domain');
     }
+    elseif (isset($_SESSION['domain_config_ui_domain'])) {
+      $this->domain = $_SESSION['domain_config_ui_domain'];
+    }
+    else {
+      $this->domain = '';
+    }
+    return $this->domain;
   }
 
   /**
    * Get the selected language ID.
+   *
+   * @return string
+   *   The domain selected language.
    */
   public function getSelectedLanguageId() {
-    if ($language = $this->request->get('domain_config_ui_language')) {
-      return $language;
+    if (isset($this->language)) {
+      return $this->language;
     }
-    if (isset($_SESSION['domain_config_ui_language'])) {
-      return $_SESSION['domain_config_ui_language'];
+    $request = $this->requestStack->getCurrentRequest();
+    if ($request->query->has('domain_config_ui_language')) {
+      $this->language = $request->get('domain_config_ui_language');
     }
+    elseif (isset($_SESSION['domain_config_ui_language'])) {
+      $this->language = $_SESSION['domain_config_ui_language'];
+    }
+    else {
+      $this->language = '';
+    }
+    return $this->language;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __sleep() {
+    $vars = $this->defaultSleep();
+    // Do not serialize static cache.
+    unset($vars['domain'], $vars['language']);
+    return $vars;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __wakeup() {
+    $this->defaultWakeup();
+    // Initialize static cache.
+    $this->domain = NULL;
+    $this->language = NULL;
   }
 
 }
